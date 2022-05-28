279ffce19b69782f79708aac5800a95bcf30e13d
PaoMo <paomo5515@qq.com>
2022-05-28 19:47:55 +0800

    learn Vue

---
+++ 01_Vue3初体验/0.CDN简介.js
@@ -0,0 +1,10 @@
+/*
+    什么是 CDN 呢？ CND 称之为内容分发网络（Content Delivery Network或 Content Distribution Network，缩写：CND）
+  它是指通过 相互连接的网络系统，利用 最靠近用户的服务器；
+  更快、更可靠地将音乐、图片、视频、应用程序及其它文件发送给用户；
+  来提供高性能、可扩展性及低成本的网络内容传递给用户；
+
+  常用的 CDN 服务器可以大致分为两种：
+    自己的 CDN 服务器：需要购买自己的 CDN 服务器，目前阿里、腾讯、亚马逊、Google等都可以购买 CDN 服务器；
+    开源的 CDN 服务器：国际上使用比较多的是 unpkg、JSDeliver、cdnjs;
+*/
