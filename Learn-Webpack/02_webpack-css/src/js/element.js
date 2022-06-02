49fa23a3b88cb475b87019038861eac8a58b405a
PaoMo <paomo5515@qq.com>
2022-06-02 10:51:52 +0800

    Learn Webpack

---
+++ Learn-Webpack/02_webpack-css/src/js/element.js
@@ -0,0 +1,5 @@
+const divEl = document.createElement("div")
+divEl.className = "title"
+divEl.innerHTML = "哈哈哈哈"
+
+document.body.appendChild(divEl)
---
+++ Learn-Webpack/02_webpack-css/src/js/format.js
@@ -0,0 +1,8 @@
+const priceFormat = function () {
+  return "￥98.89"
+}
+
+// COmmonJS导出
+module.exports = {
+  priceFormat
+}
