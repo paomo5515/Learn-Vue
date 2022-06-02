75bb19e969e49b053481da0a94a99894994b28f2
PaoMo <paomo5515@qq.com>
2022-06-02 13:36:20 +0800

    Learn Webpack

--- Learn-Webpack/02_webpack-css/src/js/element.js
+++ Learn-Webpack/02_webpack-css/src/js/element.js
@@ -1,3 +1,7 @@
+// 内联方式
+// import "css-loader!../css/style.css"
+import "../css/style.css"
+
 const divEl = document.createElement("div")
 divEl.className = "title"
 divEl.innerHTML = "哈哈哈哈"
--- Learn-Webpack/02_webpack-css/webpack.config.js
+++ Learn-Webpack/02_webpack-css/webpack.config.js
@@ -7,5 +7,20 @@ module.exports = {
     // 获取文件路径
     path: path.resolve(__dirname, "./build"),
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // 正则表达式
+        // 1.loader的写法（语法糖）
+        // loader: "css-loader"
+
+        // 2.完整写法
+        use: [
+          // { loader: "css-loader" }
+          "css-loader"
+        ]
+      }
+    ]
   }
 }
