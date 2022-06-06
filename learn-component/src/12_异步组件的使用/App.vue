72fbd148cb5089f8d8f0c9a1220e5fece46819a7
PaoMo <paomo5515@qq.com>
2022-06-06 15:05:27 +0800

    Learn Vue-Component

---
+++ learn-component/src/12_异步组件的使用/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    App组件
+    <home></home>
+    <async-category></async-category>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+import Home from "./Home.vue";
+import Loading from "./Loading.vue";
+// import AsyncCategory from "./AsyncCtaegory.vue"
+// 对 AsyncCategory 进行分包
+// const AsyncCategory = defineAsyncComponent(()=> import("./AsyncCtaegory.vue"))
+const AsyncCategory = defineAsyncComponent({
+  loader: () => import("./AsyncCtaegory.vue"),
+  // 占位组件
+  loadingComponent: Loading,
+  // 加载失败的组件
+  // errorComponent:
+  // 延迟 在我们显示 LoadingComponent 组件之前，等待多长时间
+  delay: 2000,
+});
+
+export default {
+  components: {
+    Home,
+    AsyncCategory,
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- learn-component/src/main.js
+++ learn-component/src/main.js
@@ -9,8 +9,16 @@ import { createApp } from 'vue'
 // import App from "./08_插槽的基本使用/App.vue"
 // import App from "./09_具名插槽的使用/App.vue"
 // import App from "./10_作用域插槽的使用/App.vue"
-import App from "./11_动态组件的使用/App.vue"
+// import App from "./11_动态组件的使用/App.vue"
+import App from "./12_异步组件的使用/App.vue"
+/* import { sum } from './12_异步组件的使用/utils/math'
 
+console.log(sum(20, 30)); */
+
+// 分开打包，通过 import() 导入的模块，后续 webpack 对其进行打包的时候就会进行分包的操作
+// import("./12_异步组件的使用/utils/math").then(({ sum }) => {
+//   console.log(sum(20, 30));
+// })
 
 const app = createApp(App)
 app.config.unwrapInjectedRef = true
