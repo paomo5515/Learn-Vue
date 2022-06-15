7599a13046b884ee7cf719c73ade3c62c95b798a
PaoMo <paomo5515@qq.com>
2022-06-15 12:01:36 +0800

    09_learn-vuex

--- learn-vuex/src/hooks/useState.js
+++ learn-vuex/src/hooks/useState.js
@@ -6,6 +6,8 @@ export function useState(moduleName, mapper) {
   let mapperFn = mapState
   if (typeof moduleName === "string" && moduleName.length > 0) {
     mapperFn = createNamespacedHelpers(moduleName).mapState
+  }else{
+    mapper = moduleName
   }
 
   return useMapper(mapper, mapperFn)
