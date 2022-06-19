5143fa89a68f18415493c5383865913f479b9663
PaoMo <paomo5515@qq.com>
2022-06-19 15:52:07 +0800

    Learn Vue3 Project

--- vue3-ts-cms/vue.config.js
+++ vue3-ts-cms/vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+
+  devServer: {
+    port: 6060,
+    host: '127.0.0.1',
+    open: true,
+    historyApiFallback: true
+  }
+})
